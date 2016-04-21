c22220e937871f20232b2e94f67380260738d362
maaaaagda <magdapolak96@gmail.com>
2016-04-21 23:50:44 +0200

    All algorithms made

--- src/Results.java
+++ src/Results.java
@@ -5,9 +5,12 @@ public class Results
 {
     public static void main (String[]args)
     {
-        Algorithms a = new Algorithms(3, 1000, 10);
+        Algorithms a = new Algorithms(5, 1000, 10);
         System.out.println("FIFO: " + a.FIFO());
         System.out.println("RANDOM: " + a.RANDOM());
+        System.out.println("LRU: " + a.LRU());
+        System.out.println("OPT: " + a.OPT());
+        System.out.println("OPT_APX: " + a.LRU_APX());
 
     }
 
